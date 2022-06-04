49154e0a0831160336178cbad79c98cc9e164cde
Ety-Biswangri <etybgc02@gmail.com>
2022-06-04 11:22:46 +0600

    form is designed

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#3766e8",
+          secondary: "#f5f5f5",
+          accent: "#f9f9f9",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
+        },
+      },
+      "dark",
+      "cupcake",
+    ],
+  },
+  plugins: [require("daisyui")],
+}
